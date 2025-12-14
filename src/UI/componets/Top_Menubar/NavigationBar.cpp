c4eb5a155248a604cb5884146d5de4144b6c9512
Ashley65 <45623378+Ashley65@users.noreply.github.com>
2025-12-14 15:37:36 +0000

    Add SVG icons and update CMake configuration for resource management

--- src/UI/componets/Top_Menubar/NavigationBar.cpp
+++ src/UI/componets/Top_Menubar/NavigationBar.cpp
@@ -4,10 +4,18 @@
 
 #include "UI/components/Top_Menubar/NavigationBar.h"
 
+#include <QFrame>
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QSizePolicy>
-#include <QStyle> // Added for standard icons
+#include <QStyle>
+#include <QIcon>
+#include <QSize>
+#include <QFile>
+#include <QDebug>
+#include <QDir>
+#include <QLabel>
+
 
 NavigationBar::NavigationBar(QWidget* parent) : QWidget(parent)
 {
@@ -20,48 +28,82 @@ void NavigationBar::initUi()
     m_layout->setContentsMargins(0, 0, 0, 0);
     m_layout->setSpacing(6);
 
+    constexpr int buttonSize = 28;
+
+    constexpr QSize iconSize(16, 16);
+
+
+
+
+    auto loadIcon = [](const QString &name) -> QIcon
+    {
+        QIcon ic = QIcon::fromTheme(name);
+        if (!ic.isNull())               // if theme icon exists use it
+            return ic;
+        // otherwise load from resources
+        return QIcon(QStringLiteral(":/icons/") + name + QStringLiteral(".svg"));
+    };
     // Back button
     m_backBtn = new QPushButton(this);
-    m_backBtn->setText(QString::fromUtf8("\xE2\x86\x90"));
     m_backBtn->setToolTip("Back");
-    m_backBtn->setEnabled(false);
+    m_backBtn->setAccessibleName("Back");
     m_backBtn->setFlat(true);
     m_backBtn->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    m_backBtn->setFixedSize(buttonSize, buttonSize);
+    m_backBtn->setIcon(loadIcon(QStringLiteral("arrow_back_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24")));
+    m_backBtn->setIconSize(iconSize);
     m_layout->addWidget(m_backBtn);
 
-    // Forward button
+    // Forward
     m_forwardBtn = new QPushButton(this);
-    m_forwardBtn->setText(QString::fromUtf8("\xE2\x86\x92"));
     m_forwardBtn->setToolTip("Forward");
-    m_forwardBtn->setEnabled(false);
+    m_forwardBtn->setAccessibleName("Forward");
     m_forwardBtn->setFlat(true);
     m_forwardBtn->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    m_forwardBtn->setFixedSize(buttonSize, buttonSize);
+    m_forwardBtn->setIcon(loadIcon(QStringLiteral("arrow_forward_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24")));
+    m_forwardBtn->setIconSize(iconSize);
     m_layout->addWidget(m_forwardBtn);
+
+    QLabel *sep = new QLabel("|", this);
+    sep->setFixedSize(4, buttonSize);
+    m_layout->addWidget(sep);
 
     // Refresh button
     m_refreshBtn = new QPushButton(this);
-    m_refreshBtn->setText(QString::fromUtf8("\xE2\x8E\xAF"));
     m_refreshBtn->setToolTip("Refresh");
+    m_refreshBtn->setAccessibleName("Refresh");
     m_refreshBtn->setFlat(true);
     m_refreshBtn->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    m_refreshBtn->setFixedSize(buttonSize, buttonSize);
+    m_refreshBtn->setIcon(loadIcon(QStringLiteral("refresh_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24")));
+    m_refreshBtn->setIconSize(iconSize);
     m_layout->addWidget(m_refreshBtn);
 
     // Add stretch to push buttons to the left
     m_layout->addStretch();
 
-    QString compactCss =
-    "QPushButton{"
-    "  padding: 0 6px;"      // horizontal padding per button
-    "  min-width: 0;"
-    "  min-height: 0;"
-    "  border: none;"
-    "}"
-    "QPushButton:hover {"
-   "    background: #a8c0ff;"
-   "}";
-    m_backBtn->setStyleSheet(compactCss);
-    m_forwardBtn->setStyleSheet(compactCss);
-    m_refreshBtn->setStyleSheet(compactCss);
+    const QString css =
+        "QPushButton {"
+        "  padding: 0px;"
+        "  border: none;"
+        "  border-radius: 4px;"
+        "}"
+        "QPushButton:hover {"
+        "    background: #a8c0ff;"
+        "}"
+        "QPushButton:pressed {"
+        "  background-color: rgba(255, 255, 255, 0.32);"
+        "}";
+    const QString sepCSS =
+        "QLabel{"
+        "  color: #888;"
+        "}";
+
+    m_backBtn->setStyleSheet(css);
+    m_forwardBtn->setStyleSheet(css);
+    m_refreshBtn->setStyleSheet(css);
+    sep->setStyleSheet(sepCSS);
 
     // Direct Signal Connections
     connect(m_backBtn, &QPushButton::clicked, this, &NavigationBar::backClicked);
@@ -87,12 +129,15 @@ void NavigationBar::setRefreshEnabled(bool enabled)
 
 void NavigationBar::onBackClicked()
 {
+    emit backClicked();
 }
 
 void NavigationBar::onForwardClicked()
 {
+    emit forwardClicked();
 }
 
 void NavigationBar::onRefreshClicked()
 {
+    emit refreshClicked();
 }
